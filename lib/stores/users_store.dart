7d6cc3df7b5c9505ff8ad30213453a457d9c8999
Aurimas Deimantas <aurimas@fertilemind.app>
2022-01-06 20:20:47 +0700

    feat: include firebase authentication

--- lib/stores/users_store.dart
+++ lib/stores/users_store.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart' as f_auth;
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:secretum/main.dart';
@@ -11,14 +12,16 @@ import 'package:secretum/services/storage_service.dart';
 
 class UsersStore extends ChangeNotifier {
   final EncryptionService _encryptionService;
+  final f_auth.FirebaseAuth _firebaseAuth;
   final FireUsersService _fireUsersService;
   final StorageService _storageService;
   final List<StreamSubscription?> _streamSubscriptions = [];
 
   User? user;
 
-  UsersStore()
+  UsersStore({f_auth.FirebaseAuth? firebaseAuth})
       : this._encryptionService = GetIt.instance<EncryptionService>(),
+        this._firebaseAuth = firebaseAuth ?? f_auth.FirebaseAuth.instance,
         this._fireUsersService = GetIt.instance<FireUsersService>(),
         this._storageService = GetIt.instance<StorageService>();
 
@@ -28,9 +31,10 @@ class UsersStore extends ChangeNotifier {
   }
 
   Future<void> initUserViaSecretKey(String secretKey) async {
-    final User? user = await _getUserBySecretKey(secretKey);
+    final User? user = await _fireUsersService.getUserBySecretKey(secretKey);
 
     if (user != null) {
+      await _firebaseAuth.signInAnonymously();
       updateUserLocally(user);
       await _storageService.initSecretKey(secretKey);
       _listenToUserByUserId(user.id);
@@ -39,66 +43,52 @@ class UsersStore extends ChangeNotifier {
 
   Future<void> initUserOnAppStart() async {
     final String? secretKey = await _storageService.getSecretKey();
-
-    if (secretKey != null) {
-      final User? user = await _fireUsersService.getUserBySecretKey(secretKey);
-      // If user exists, initialise listeners
-      if (user != null) {
-        // SecretKey is already initialised in the StorageService. We need to make
-        // sure that EncryptionService `key` is also up to date
-        _encryptionService.updateSecretKey(secretKey);
-        // Update user immediately in Store, because listening might take 0.x second to retrieve the
-        // user from stream
-        updateUserLocally(user);
-        _listenToUserByUserId(user.id);
-      }
-      // If user doesn't exist, something went wrong, because SecretKey is in storage
-      // yet there is no user found in Firestore. Investigate.
-      else {
-        loggingService.log(
-          'UsersStore._initAndListenToUser: User from firestore is null',
-          logType: LogType.error,
-        );
-      }
-    } else {
+    if (secretKey == null) {
       loggingService.log('UsersStore.initUserOnAppStart: SecretKey is null - user is not logged in');
+      return;
     }
-  }
 
-  Future<User?> _getUserBySecretKey(String secretKey) async {
     final User? user = await _fireUsersService.getUserBySecretKey(secretKey);
+    // If user doesn't exist, something went wrong, because SecretKey is in storage
+    // yet there is no user found in Firestore. Investigate.
+    if (user == null) {
+      loggingService.log('UsersStore._initAndListenToUser: User from firestore is null', logType: LogType.error);
+      return;
+    }
 
-    return user;
+    await _firebaseAuth.signInAnonymously();
+    // SecretKey is already initialised in the StorageService. We need to make
+    // sure that EncryptionService `key` is also up to date
+    _encryptionService.updateSecretKey(secretKey);
+    // Update user immediately in Store, because listening might take 0.x second to retrieve the
+    // user from stream
+    updateUserLocally(user);
+    _listenToUserByUserId(user.id);
   }
 
-  Future<bool> registerUser(User user) async {
-    await _storageService.initSecretKey(user.sensitiveInformation.secretKey);
+  Future<bool> registerUser(User userToRegister) async {
+    await _storageService.initSecretKey(userToRegister.sensitiveInformation.secretKey);
+    final String? userId = await _fireUsersService.registerUser(userToRegister);
 
-    final String? userId = await _fireUsersService.registerUser(user);
-
-    // If userId is not null, fetch user from firestore and start listener
-    if (userId != null) {
-      final User? user = await _getUserById(userId);
-
-      if (user != null) {
-        loggingService.log('UsersStore.registerUser: User registered. UserId: $userId');
-        updateUserLocally(user);
-        _listenToUserByUserId(userId);
-
-        return true;
-      }
-      // If userId is not null but user from firestore is null, something went wrong.
-      // Investigate
-      else {
-        loggingService.log('UsersStore.registerUser: User is null from Firestore', logType: LogType.error);
-        return false;
-      }
-    }
-    // If userId is null
-    else {
+    // If userId is not null but user from firestore is null, something went wrong. Investigate
+    if (userId == null) {
       loggingService.log('UsersStore.registerUser: User is null', logType: LogType.error);
       return false;
     }
+
+    final User? user = await _getUserById(userId);
+    if (user == null) {
+      loggingService.log('UsersStore.registerUser: User is null from Firestore', logType: LogType.error);
+      return false;
+    }
+
+    loggingService.log('UsersStore.registerUser: User registered. UserId: $userId');
+
+    await _firebaseAuth.signInAnonymously();
+    updateUserLocally(user);
+    _listenToUserByUserId(userId);
+
+    return true;
   }
 
   void _listenToUserByUserId(String userId) {
@@ -122,6 +112,7 @@ class UsersStore extends ChangeNotifier {
     });
     _streamSubscriptions.clear();
 
+    _firebaseAuth.signOut();
     updateUserLocally(null);
   }
 }
